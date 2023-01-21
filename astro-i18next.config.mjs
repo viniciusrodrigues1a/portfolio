09968924073db6055b7346cd76ed6f18f21d379a
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2023-01-20 22:09:39 -0300

    build: configure i18n with astro-i18next

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLocale: "pt-BR",
+  locales: ["pt-BR", "en"],
+  namespaces: ["home"],
+  defaultNamespace: "home",
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import astroI18Next from "astro-i18next";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [astroI18Next()],
+});
