3e8fa8d17deae6b284599c6204662293a816db26
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2023-06-09 20:29:05 -0300

    feat: add ThemeSwitchButtonUnstyledWrapper and ThemeSwitchHiddenInput

--- src/components/ThemeSwitch/index.ts
+++ src/components/ThemeSwitch/index.ts
@@ -1,4 +1,9 @@
 import ThemeSwitchButtonComponent from "./ThemeSwitchButton.Component.astro";
 import ThemeSwitchHiddenInputComponent from "./ThemeSwitchHiddenInput.Component.astro";
+import ThemeSwitchButtonUnstyledWrapper from "./ThemeSwitchButtonUnstyledWrapper.Component.astro";
 
-export { ThemeSwitchButtonComponent, ThemeSwitchHiddenInputComponent };
+export {
+  ThemeSwitchButtonComponent,
+  ThemeSwitchHiddenInputComponent,
+  ThemeSwitchButtonUnstyledWrapper,
+};
