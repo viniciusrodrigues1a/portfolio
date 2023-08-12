0430a537f76ea097aa314b23caad4e310220fd14
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2023-08-12 00:27:10 -0300

    refactor: extract html from home page to section components

---
+++ src/components/_page-components/home/index.ts
@@ -0,0 +1,11 @@
+import IntroSectionComponent from "./IntroSection.Component.astro";
+import AboutMeSectionComponent from "./AboutMeSection.Component.astro";
+import ExperienceSectionComponent from "./ExperienceSection.Component.astro";
+import FooterComponent from "./Footer.Component.astro";
+
+export {
+  IntroSectionComponent,
+  AboutMeSectionComponent,
+  ExperienceSectionComponent,
+  FooterComponent,
+};
