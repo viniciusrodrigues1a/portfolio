c5b22844836c8d2ae31266dfce7f076a263089d2
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2023-09-26 17:25:59 -0300

    build: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // https://astro.build/config
 import { defineConfig } from "astro/config";
 import astroI18Next from "astro-i18next";
+import tailwind from "@astrojs/tailwind";
 
 import compress from "astro-compress";
 
 export default defineConfig({
-  integrations: [astroI18Next(), compress()],
+  integrations: [astroI18Next(), compress(), tailwind()],
 });
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: [
+          "Roboto Mono",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
