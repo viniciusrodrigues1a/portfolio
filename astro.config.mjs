154b5483c82897d4b76df4342704e64452595350
Vinícius Rodrigues de Aro <viniciusrodrigues.aro@gmail.com>
2023-12-27 17:40:20 -0300

    fix: i18n project detail urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from "astro/config";
 import astroI18Next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 import compress from "astro-compress";
 
 export default defineConfig({
-  integrations: [astroI18Next(), compress(), tailwind()],
+  integrations: [astroI18Next(), compress(), tailwind(), mdx()],
 });
